ab56a5f3c96cc455030d420160205ba118c84f36
David <fatokundavid2@gmail.com>
2023-06-11 17:23:29 -0600

    complete refactor and testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,15 @@
-let args = process.argv;
-args = args.slice(2);
-let url = 'https://api.thecatapi.com/v1/breeds/search?q=' + args[0];
+const request = require('request')
+let url = 'https://api.thecatapi.com/v1/breeds/search?q=';
 
-const request = require('request');
-request(url, (error, response, body) => {
-    if (error) {
-      console.log('error:', error); // Print the error if one occurred
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    } 
-    const data = JSON.parse(body);
-    if(data.length === 0) {
-      console.log('Unable to find: ' + args[0]);
-      return;
-    }
-    console.log(data[0].description);
-});
+const fetchBreedDescription = function (breedName, callback) {
+    request(url + breedName, (error, response, body) => {
+        const data = JSON.parse(body);
+        let desc = null;
+        if(data[0] !== undefined || data.length !== 0){
+            desc = data[0].description;
+        }
+        callback(error, desc)
+    });
+};
+
+module.exports = {fetchBreedDescription};
